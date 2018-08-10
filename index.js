618d99f4d3498f52670962c761e928b44b291d50
Giang Peter <giangmd93@gmail.com>
2018-08-10 16:52:45 +0700

    Init name package

---
+++ index.js
@@ -0,0 +1,8 @@
+var nanjAPI = require('./lib/nanjAPI');
+
+// dont override global variable
+if (typeof window !== 'undefined' && typeof window.nanjAPI === 'undefined') {
+    window.nanjAPI = nanjAPI;
+}
+
+module.exports = nanjAPI;
---
+++ test/index.js
@@ -0,0 +1,20 @@
+//Require the dev-dependencies
+let chai = require('chai')
+let chaiHttp = require('chai-http')
+let server = require('../index')
+let should = chai.should()
+// let should = chai.expect()
+
+chai.use(chaiHttp)
+
+describe('* TESTING PACKAGE *', () => {
+	
+});
+
+// describe('* TESTING WALLET *', () => {
+// 	require('./test-wallet.js');
+// });
+
+// describe('* TESTING TRANSACTION *', () => {
+// 	require('./test-transaction.js');
+// });
