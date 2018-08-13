48466b3fa052012f8b9491951f3d3c023413c7d6
Giang Peter <giangmd93@gmail.com>
2018-08-13 17:32:23 +0700

    add import wallet

--- middleware/validate-wallet-import.js
+++ middleware/validate-wallet-import.js
@@ -5,44 +5,35 @@ var ethUtil 	= require('ethereumjs-util');
 ethUtil.crypto 	= require('crypto');
 ethUtil.scrypt 	= require('scryptsy');
 
-module.exports = function (req, res, next) {
-
-	if (req.files === null) {
-		return res.status(422).json({message: "Keystore is required."});
-	}
-	
-	var jsonKeystore = null;
-	if (typeof req.files == 'undefined') {
-		if (typeof req.body.keystore == 'undefined') {
-			return res.status(422).json({message: "Keystore is required."});
-		}
-
-		jsonKeystore = req.body.keystore;
-	} else {
-		jsonKeystore = (typeof req.files.keystore == 'undefined')?null:req.files.keystore.data;
-	}
-
-	if (jsonKeystore === null) {
-		return res.status(422).json({message: "Keystore is required."});
-	}
-
-	var keystore = JSON.parse(jsonKeystore.toString('ascii').toLowerCase());
-    var password = req.body.password;
+module.exports = function (keystore, password) {
+	const valid = {status: 200, message: ''}
 
 	if ((typeof keystore == "undefined") || (keystore.length == 0)) {
-    	return res.status(422).json({message: "Keystore is required."});
+		valid.status = 422
+		valid.message = "Keystore is required."
+
+		return valid
 	} else if ((typeof password == "undefined") || (password.length == 0)) {
-    	return res.status(422).json({message: "Password is required."});
+    	valid.status = 422
+		valid.message = "Password is required."
+
+		return valid
 	} else {
 		// validate keystore 
-	    var derivedKey;
-    	var kdfparams;
+	    var derivedKey = ''
+    	var kdfparams = {}
     	if (typeof keystore.crypto == 'undefined') {
-    		return res.status(422).json({message: "Wallet format unknown or unsupported!"});
+    		valid.status = 403
+			valid.message = "Wallet format unknown or unsupported!"
+
+			return valid
     	} else if (keystore.crypto.kdf == 'pbkdf2') {
 	    	kdfparams = keystore.crypto.kdfparams
 	        if (kdfparams.prf != 'hmac-sha256') {
-	        	return res.status(422).json({message: "Unsupported parameters to PBKDF2"});
+	        	valid.status = 403
+				valid.message = "Unsupported parameters to PBKDF2"
+
+				return valid
 	        }
 
 	        derivedKey = ethUtil.crypto.pbkdf2Sync(new Buffer(password), new Buffer(kdfparams.salt, 'hex'), kdfparams.c, kdfparams.dklen, 'sha256');
@@ -50,20 +41,30 @@ module.exports = function (req, res, next) {
 	    	kdfparams = keystore.crypto.kdfparams;
         	derivedKey = ethUtil.scrypt(new Buffer(password), new Buffer(kdfparams.salt, 'hex'), kdfparams.n, kdfparams.r, kdfparams.p, kdfparams.dklen);
 	    } else {
-	    	return res.status(422).json({message: "Wallet format unknown or unsupported!"});
+	    	valid.status = 403
+			valid.message = "Wallet format unknown or unsupported!"
+
+			return valid
 	    }
 
 		// validate password
         if (password.length < 9) {
-	    	return res.status(422).json({message: "Your password must be at least 9 characters. Please ensure it is a strong password."});
+	    	valid.status = 422
+			valid.message = "Your password must be at least 9 characters. Please ensure it is a strong password."
+
+			return valid
 		}
 
 		var ciphertext = new Buffer(keystore.crypto.ciphertext, 'hex');
 		var newMac = ethUtil.sha3(Buffer.concat([derivedKey.slice(16, 32), ciphertext]));
 
 		if (newMac.toString('hex') !== keystore.crypto.mac) {
-			return res.status(422).json({message: "Key derivation failed - possibly wrong passphrase."});
+			valid.status = 422
+			valid.message = "Key derivation failed - possibly wrong passphrase."
+
+			return valid
 		}
-		return next();
+		
+		return valid
 	}
 };
