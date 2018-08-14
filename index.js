a441fc20ce0f8e3ebf53ac680e78ef4be54e0926
Giang Peter <giangmd93@gmail.com>
2018-08-14 19:27:41 +0700

    restructor

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var config = require('./config');
-var nanjAPI = require('./lib/nanjAPI');
-
-// dont override global variable
-if (typeof window !== 'undefined' && typeof window.nanjAPI === 'undefined') {
-    window.nanjAPI = nanjAPI;
-}
-
-module.exports = nanjAPI;
+module.exports = {
+  wallet: require('./lib/wallet.js'),
+  transaction: require('./lib/transaction.js')
+};
